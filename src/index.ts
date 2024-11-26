8f53ffc4c01d740645d11dd2e9737061d37e01eb
Kevin Minutti <minutti70@gmail.com>
2024-11-26 10:40:48 -0500

    switch to ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as d3 from "d3";
+
+const svg = d3.select("body")
+  .append("svg")
+  .attr("width", 800)
+  .attr("height", 600);
+
+svg.append("circle")
+  .attr("cx", 400)
+  .attr("cy", 300)
+  .attr("r", 50)
+  .style("fill", "blue");

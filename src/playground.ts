78341056b43a791a0d11bb48c22ae299f6df55d6
Kevin Minutti <minutti70@gmail.com>
2024-11-28 13:02:54 -0500

    d3 shapes

--- src/playground.ts
+++ src/playground.ts
@@ -2,11 +2,25 @@ import * as d3 from "d3";
 
 const svg = d3.select("body")
   .append("svg")
-  .attr("width", 800)
-  .attr("height", 600);
+  .attr("width", 900)
+  .attr("height", 700);
 
 svg.append("circle")
   .attr("cx", 400)
   .attr("cy", 300)
   .attr("r", 50)
-  .style("fill", "green");
+  .style("fill", "blue");
+
+svg.append("circle")
+  .attr("cx", 500)
+  .attr("cy", 400)
+  .attr("r", 50)
+  .style("fill", "blue");
+
+svg.append('rect')
+  .attr('x', 10)
+  .attr('y', 120)
+  .attr('width', 600)
+  .attr('height', 40)
+  .attr('stroke', 'black')
+  .attr('fill', '#69a3b2');
